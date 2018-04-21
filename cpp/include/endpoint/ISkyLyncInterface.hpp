c23f4dfb8b3a03951b2f230906a330d6b1b1e409
Bartosz Nawrot <nawbar23@gmail.com>
2018-04-21 17:20:56 +0200

    Remove unused files

--- cpp/include/endpoint/ISkyLyncInterface.hpp
+++
@@ -1,8 +0,0 @@
-// =========== roboLib ============
-// ===  *** BARTOSZ NAWROT ***  ===
-// ================================
-#ifndef ISKYLYNCINTERFACE_HPP
-#define ISKYLYNCINTERFACE_HPP
-
-
-#endif // ISKYLYNCINTERFACE_HPP
